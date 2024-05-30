c0fb72a8103020bda9d524238a62e6c7e50f7180
cuidadoaltoque <cuidadoaltoque.project@gmail.com>
2024-05-30 00:23:51 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,64 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Cuál es el primer paso en la evaluación de una víctima en primeros auxilios?",
+        answer: "B. Verificar la respuesta de la víctima",
+        options:[
+            "A. Llamar al servicio de emergencia",
+            "B. Verificar la respuesta de la víctima",
+            "C. Comprobar la respiración",
+            "D. Controlar el sangrado"
+
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "“Conducta P.A.S.” que consiste en adoptar las siguientes medidas de primeros auxilios:",
+        answer: "B. Proteger, Avisar, Socorrer",
+        options:[
+            "A. Preguntar, Actuar, Sanar",
+            "B. Proteger, Avisar, Socorrer",
+            "C. Protocolizar, Auxiliar, Situar",
+            "D. Prevenir, Aliviar, Solucionar"
+            
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Cuáles son las principales señales objetivas que se deben valorar cuando nos encontramos antes un herido?",
+        answer: "C. Consciencia, respiración, pulso",
+        options:[
+            "A. Conciencia, respiración, pulso",
+             "B. Hemorragias, fracturas y quemaduras",
+             "C. Consciencia, respiración, pulso",
+             "D. Consciencia, fracturas y hemorragias"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "Qué es lo que NO debe hacerse ante una herida grave?",
+        answer: "D. Quitar los cuerpos extraños visibles, aunque estén profundos",
+        options:[
+            "A. No se debe tocar la herida",
+             "B. Colocar encima una compresa estéril o paño limpio",
+             "C. Trasladar al herido a un centro apropiado",
+             "D. Quitar los cuerpos extraños visibles, aunque estén profundos"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Cuál es la señal más común de un ataque cardíaco?",
+        answer: "D. Dolor opresivo en el pecho",
+        options:[
+            "A. Dolor de cabeza severo",
+            "B. Dificultad para respirar",
+            "C. Dolor agudo en el abdomen",
+            "D. Dolor opresivo en el pecho"
+        ]
+    },
+
+];
